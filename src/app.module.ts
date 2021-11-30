73b5c3281b68c485a60e76231f90abc54398db13
floriansmeyers <floriansmeyers@gmail.com>
2021-11-30 19:01:26 +0100

    Update app.module.ts
    add MongooseModule and URL string

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    TodoModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://nodejs-workshop:nodejs-workshop@cluster0.096bi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
